f684575fd8e3ba3be75edc641a3cb5373295399d
kyle <ashkyw@gmail.com>
2026-04-02 18:09:08 -0500

    added leafnodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,12 +6,28 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError("children will override this")
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        if self.props is None or if self.props == "":
+        if self.props is None:
             return ""
         return " " + " ".join([f'{key}="{value}"' for key, value in self.props.items()])
 
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,39 +1,69 @@
 import unittest
-
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html(self):
-        props = {
-            "href": "https://www.google.com",
-            "target": "_blank"
-        }
-        node = HTMLNode(props)
-        self.assertIsNotNone(node)
-        self.assertTrue(node) == '"href": "https://www.google.com", "target": "_blank"'
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
 
-        props = ""
-        node = HTMLNode(props)
-        self.assertIsNotNone(node)
-        self.assertTrue(node) == ""
-
-        props = "Hello, world"
-        node = HTMLNode(props)
-        self.assertIsNotNone(node)
-        self.assertTrue(node) == ValueError
-
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
 
     def test_repr(self):
-        tag = "<a>"
-        value = "Hello, world"
-        children = "1"
-        props = {
-            "href": "https://www.google.com",
-            "target": "_blank"
-        }
-        node = HTMLNode(tag, value, children, props)
-        self.assertTrue(node) == 'HTMLNode(<a>, Hello, world, 1, href: "https://www.google.com", target: "_blank")'
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
 
 if __name__ == "__main__":
     unittest.main()
